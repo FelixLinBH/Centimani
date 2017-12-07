fbacb0ebae9983a2864f37836d910fe5f642c757
felix.lin <fly_81211@hotmail.com>
2017-12-07 16:04:17 +0800

    Added init setting instruction.

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 //centimani new controller test
-var setting = require('./setting.json')
+
 var core = require('./lib')
-var key = Object.keys(setting).map(function(el){
-	return el;
-})
-
-
+var setting,key;
 
 var program = require('commander');
 program
-  .command('init [target] [type]')
-  .description('init new command')
-  .action(function (target,type,options) {
-    console.log(target);
+  .command('init')
+  .description('init new setting json')
+  .action(function (options) {
+
+    core.run('','','','init').then(
+        function fulfilled(){
+          console.log("success created setting.json file.")
+          return;
+        },
+        function rejected(reason){
+          console.log("wrong =>" + reason)
+          return;
+        }
+      );
   });
+
+if (process.argv[process.argv.length - 1] != 'init') {
+  try {
+   setting= require('./setting.json')
+  }
+  catch (err) {
+
+      program.parse(process.argv);
+      console.error( "Setting.json is not found!! Please use init instruction.");
+      return;
+  }
+  key = Object.keys(setting).map(function(el){
+    return el;
+  })
+}else{
+  program.parse(process.argv);
+  return;
+}
+
 
 
 program
@@ -21,17 +46,25 @@ program
   .description('run new commands with ' + key.toString() )
   // .option("-s, --setup_mode [mode]", "Which setup mode to use")
   .action(function(target, name, options){
-  	console.log(name);
+    if (target === undefined) {
+      console.log('target command is missed!');
+      return;
+    }
+    if (name === undefined) {
+      console.log('name is missed!', target);
+      return;
+    }
   	if (key.indexOf(target) === -1) {
   		console.log('%s command not found.', target);
   		return;
   	}
+    
     console.log('Begin creating %s %s ', name,target);
     Object.keys(setting[target]).map(function(el){
       core.run( name,el,setting[target][el],'new')
       .then(
         function fulfilled(){
-          console.log("success")
+          console.log("success created %s %s",name,target);
         },
         function rejected(reason){
           console.log("wrong =>" + reason)
@@ -44,6 +77,7 @@ program
   });
 
 program.parse(process.argv);
+
 
 
 
